13631b4484f91165831ca2c866a6b8b1a56708d5
ryan <ryan.gibson@alchemybet.com>
2019-03-29 12:34:27 +0000

    Added Line interconnection test to trig,js

--- framework/trig.js
+++ framework/trig.js
@@ -227,6 +227,38 @@ define(function(require) {
 	};
 
 
+
+
+	Trig.CalcLineInterconnection = function(p0_x,p0_y,p1_x,p1_y,p2_x,p2_y,p3_x,p3_y){
+
+		// converted from stack overflow C# example. Amazing stuff...
+		
+		//console.log("CalcLineInterconnection.")
+		//console.log("ln 1: " + p0_x +","+ p0_y + " to " +  p1_x +","+ p1_y);
+		//console.log("ln 2: " + p2_x +","+ p2_y + " to " +  p3_x +","+ p3_y);
+
+		s1_x = p1_x-p0_x;
+		s1_y = p1_y-p0_y;
+		s2_x = p3_x-p2_x;
+		s2_y = p3_y-p2_y;
+
+		s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
+	    t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+	    //if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+	    if (s > 0.001 && s < 0.999 && t > 0.001 && t < 0.999)
+		{
+	        // Collision detected- we can optionally return X and Y to get precise point of collision
+			var intX = p0_x + (t * s1_x);
+			var intY = p0_y + (t * s1_y);
+			//console.log(intX + " " + intY);
+
+	        return true;
+	    }
+		return false; // No collision
+	}
+
+
 	
 	return Trig;
 });
